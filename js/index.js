cb67c95b1dcc08fd4f5597de7953f77f2e8d16ac
bitsypookums24 <kiraal@att.net>
2019-08-15 19:07:08 -0700

    Homework19

---
+++ js/index.js
@@ -0,0 +1,4 @@
+ScrollReveal().reveal('.card');
+ScrollReveal().reveal('.card-title', { delay: 100 });
+ScrollReveal().reveal('.card-img-top', { delay: 100 });
+ScrollReveal().reveal('.card-text', { delay: 300 });
